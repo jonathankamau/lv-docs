f62de333fbfa609139b0f4b4958ba94c056f4d94
jonathankamau <jonathan.kamau@andela.com>
2017-09-14 10:54:17 +0300

    chore(command-line-script): command line script that contains commands

---
+++ shell-script.sh
@@ -0,0 +1,8 @@
+mkdir first-directory second-directory
+mv second-directory first-directory
+cd first-directory
+touch first-file
+cp first-file second-file
+ls  > second-file
+cat second-file | nano third-file
+wc -l < second-file
